a60abb99f501a3911051da4f15da18327bcb8615
Pedro Motta <pedromotta1d2015@gmail.com>
2023-03-12 21:51:09 -0300

    add review section

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,26 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let videoBtn = document.querySelectorAll('.vid-btn');
 
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
